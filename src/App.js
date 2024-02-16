394cbbeb75e760a41dfb8e455ef5fda276db2be4
WajihaAbrar <wajihaabrar@gmail.com>
2024-02-16 20:50:10 +0000

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import dictionary from "./dictionary.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={dictionary}
+          className="App-logo img-fluid"
+          alt="Dictionary App Logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
