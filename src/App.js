01168047d30068f736acc364ba85bc862cece215
WajihaAbrar <wajihaabrar@gmail.com>
2024-02-17 10:58:09 +0000

    Added the search form in dictionary component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import Dictionary from "./Dictionary";
 import dictionary from "./dictionary.png";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={dictionary}
-          className="App-logo img-fluid"
-          alt="Dictionary App Logo"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={dictionary}
+            className="App-logo img-fluid"
+            alt="Dictionary App Logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Wajiha Abrar</small>
+        </footer>
+      </div>
     </div>
   );
 }
