8489691e1da3d9d08ef573e69e90d3b4f28ae8cd
WajihaAbrar <wajihaabrar@gmail.com>
2024-02-18 11:23:01 +0000

    Added the Example component.

---
+++ src/Example.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Example(props) {
+  if (props.example) {
+    return (
+      <div className="Example">
+        <strong>Example:</strong> <em>{props.example}</em>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default Example;
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import Example from "./Example";
 
 function Meaning(props) {
   return (
@@ -11,7 +12,7 @@ function Meaning(props) {
             <p>
               <strong>Definition:</strong> {definition.definition}
               <br />
-              <strong>Example:</strong> <em>{definition.example}</em>
+              <Example example={definition.example} />
               <br />
               <Synonyms synonyms={definition.synonyms} />
             </p>
