5f49f9a513dd7f1a062219b766dbffe5c9c737ca
WajihaAbrar <wajihaabrar@gmail.com>
2024-02-18 11:13:17 +0000

    Added the Synonyms and Phonetics component.

---
+++ src/Phonetic.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Phonetic(props) {
+  console.log(props.phonetic);
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
+}
+
+export default Phonetic;
